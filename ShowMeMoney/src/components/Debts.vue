9f5c5aab88978c393af64c2a093ee91f1b5d04fe
damian <damianjankowski111@gmail.com>
2019-02-09 00:21:45 +0100

    Debts cleanup

--- ShowMeMoney/src/components/Debts.vue
+++ ShowMeMoney/src/components/Debts.vue
@@ -24,30 +24,6 @@ export default {
     },
     beforeCreate () {
         this.$store.dispatch('loadDebts')
-    },
-    methods: {
-        trimId (id) {
-            let dots = ''
-            if (id.length > 10) dots = '.. '
-            return id.substring(0, 8) + dots
-        },
-        setDebt (debt) {
-            // this.$store.commit('SET_DEBT', debt)
-            this.$store.commit('SET_DEBT_ID', debt.debtId)
-            this.$store.dispatch('loadDebt')
-        },
-        deleteDebt (debtId) {
-            this.$store.dispatch('deleteDebt', debtId)
-        },
-        getDebtorDebts (debtor) {
-            this.$store.dispatch('getDebtorDebts', debtor)
-        }
     }
 }
 </script>
-
-<style>
-
-
-
-</style>
